28c2263a6786aaa4b4f43083b1080a1dfab7b0cc
xuxj <760409558@qq.com>
2025-06-30 17:21:17 +0800

    feat: predict chore: update labels

--- predict.py
+++ predict.py
@@ -18,8 +18,7 @@ def predict(model:NERModel, text:str, tokenizer: BertTokenizerFast, label_dict: 
     output = model.forward(inputs['input_ids'], inputs['attention_mask'])
     label_ids = torch.argmax(output, dim=-1).view(-1) #(n)
     result = convert_label_to_entities(tokens, label_ids, label_dict)
-    print(result)
-    pass
+    return result
 
 def convert_label_to_entities(tokens: list[str], label_ids: list|torch.Tensor, label_dict:dict):
     id_to_label = [''] * len(label_dict)
@@ -28,18 +27,22 @@ def convert_label_to_entities(tokens: list[str], label_ids: list|torch.Tensor, l
     
     idx = 0
     label_tokens = {}
+    combined_tokens = []
     for id in label_ids.tolist():
         label = id_to_label[id]
+        token = tokens[idx]
+        combined_tokens.append(token + label)
         if label is None or label == 'O':
             idx += 1
             continue
-        token = tokens[idx]
+        
         idx += 1
         if label.startswith('B-') or label.startswith('I-'):
             label_tokens[token] = label
     
     return {
-        "ner_label": label_tokens
+        "ner_label": label_tokens,
+        "combined_tokens": combined_tokens,
     }
 
 if __name__ == '__main__':
@@ -56,4 +59,4 @@ if __name__ == '__main__':
     text = '时间：2020 年 08 月 06 日 13 时 50 分至 14 时 40 分​ 地点：哈尔滨市南岗区荣市派出所​ 被询问人：林悦晴 性别：女 年龄：28 出生日期：1992 年 8 月 12 日​ 身份证号码：230103199208129876 工作单位及职务：图书馆管理员 联系方式：18845012345​ 讯问内容：在图书馆值班时，一名男子大声接听电话，我上前提醒保持安静，他不仅不听，还故意提高音量，我再次制止，他将咖啡泼在书架上，我阻拦时被他推搡，其他读者报警。'
 
     result = predict(model, text, tokenizer, label_dict)
-    print(result)
+    print(result['combined_tokens'])
